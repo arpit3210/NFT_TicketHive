504ee6ba5305f24203fd5f9b42a08e3ca15352cd
Arpit Singh <arpitsinghthakur321@gmail.com>
2023-04-29 15:30:25 +0530

    add some code of line in css folders

--- javascript/app.js
+++ javascript/app.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
     web3 = new Web3(ethereum);
     console.log("Connection Object: ",web3)
    // 0x00C1d5c2000f3121b7aCFDF92b27397e1932Ad80                    sepoliya
-
+//0x956Ea303fb3EA898C779151218751A152A65FB2b            spinixx 2.0
    // 0x5FbDB2315678afecb367f032d93F642f64180aa3
-    const contractAddress = "0x00C1d5c2000f3121b7aCFDF92b27397e1932Ad80";
+    const contractAddress = "0x956Ea303fb3EA898C779151218751A152A65FB2b";
     // const contractAbi = [
     //     {
     //         "inputs": [
@@ -90,79 +90,79 @@ $(document).ready(function () {
     // ];
 
     const contractAbi =  [
-        {
-          "inputs": [],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "_PersonID",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_Name",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_Experience",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_Orgnanization_or_CollegeName",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "_EmailID_AND_PhoneNo",
-              "type": "string"
-            }
-          ],
-          "name": "newPersonID",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "name": "personidDetails",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "Name",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "Experience",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "Orgnanization_or_CollegeName",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "EmailID_AND_PhoneNo",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        }
-      ];
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_PersonID",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_Name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_Experience",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_Orgnanization_or_CollegeName",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_EmailID_AND_PhoneNo",
+            "type": "string"
+          }
+        ],
+        "name": "newPersonID",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "name": "personidDetails",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "Name",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "Experience",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "Orgnanization_or_CollegeName",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "EmailID_AND_PhoneNo",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
 
     myContract = new web3.eth.Contract(contractAbi, contractAddress);
     console.log("Contract Object: ",myContract);
